7f6bd6eb84ee252808d65062e29cfc8f35cacc99
Дмитрий <dimas.savin1990@gmail.com>
2022-11-22 22:51:44 -0500

    almost finish task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    body: document.querySelector('body'),
+    start: document.querySelector('[data-start]'),
+    stop: document.querySelector('[data-stop')
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+refs.start.addEventListener('click', startBgChange);
+refs.stop.addEventListener('click', stopBgChange);
+
+
+function startBgChange() {
+    onChangeBgk();
+    refs.stop.disabled = false;
+    refs.stop.disabled = true;
+    setInterval(() => {
+    onChangeBgk();
+    }, 1000);
+    
+};
+
+function stopBgChange() {
+    clearInterval(1);
+};
+
+function onChangeBgk() {
+refs.body.style.backgroundColor = getRandomHexColor();
+}
